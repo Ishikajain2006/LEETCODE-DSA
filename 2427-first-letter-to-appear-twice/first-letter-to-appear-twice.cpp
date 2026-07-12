3d88f671d9298be2c511da8caac8c1dea66845a3
ISHIKA JAIN <pawanishika10@gmail.com>
2026-07-12 22:50:34 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (86.68%) - LeetSync

---
+++ 2427-first-letter-to-appear-twice/first-letter-to-appear-twice.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    char repeatedCharacter(string s) {
+        int freq[26];
+        for(char ch : s ){
+            freq[ch-'a']++;
+            if(freq[ch-'a']>1){
+                return ch;
+            }
+        }
+        return 'a';
+    }
+};
