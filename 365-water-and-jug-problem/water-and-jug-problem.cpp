dcea262e268b2d2e170be9c9ad37aa8cea5a01f7
ISHIKA JAIN <pawanishika10@gmail.com>
2026-07-17 23:43:09 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (51.19%) - LeetSync

--- 365-water-and-jug-problem/water-and-jug-problem.cpp
+++ 365-water-and-jug-problem/water-and-jug-problem.cpp
@@ -3,15 +3,13 @@ public:
     int gcd(int a , int b){
         while(b!=0){
             int temp = a%b ;
-            a=b ;
+            a = b ;
             b = temp ;
         }
         return a ;
     }
     bool canMeasureWater(int x, int y, int target) {
         if(target>x+y) return false ;
-        int div = gcd(x,y);
-        if(target%div==0) return true ;
-        return false ;
+        return target % gcd(x,y) == 0 ;
     }
 };
