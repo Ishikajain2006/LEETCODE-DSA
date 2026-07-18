d9da156802f3ffec826516fd3339d86f5d0df9b4
ISHIKA JAIN <pawanishika10@gmail.com>
2026-07-18 18:23:48 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (21.38%) - LeetSync

---
+++ 3447-clear-digits/clear-digits.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string clearDigits(string s) {
+        stack<char>st ;
+        for(char ch : s){
+            if(ch >='0' && ch <= '9' ){
+                if(!st.empty()) st.pop();
+            }
+            else st.push(ch) ;
+        }
+        string ans = "";
+        while(!st.empty()){
+            ans += st.top();
+            st.pop();
+        }
+        reverse(ans.begin() , ans.end()) ;
+        return ans ;
+    }
+};
