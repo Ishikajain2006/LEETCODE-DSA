0910f5b36a1899b2845ec426de42dec5417b282f
ISHIKA JAIN <pawanishika10@gmail.com>
2026-08-28 00:33:00 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (89.35%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
@@ -11,15 +11,36 @@
  */
 class Solution {
 public:
-    void display(TreeNode* root , vector<int>&inorder){
-        if(root==NULL) return ;
-        display(root->left , inorder );
-        inorder.push_back(root->val);
-        display(root->right , inorder);
-    }
+    // void display(TreeNode* root , vector<int>&inorder){
+    //     if(root==NULL) return ;
+    //     display(root->left , inorder );
+    //     inorder.push_back(root->val);
+    //     display(root->right , inorder);
+    // }
     vector<int> inorderTraversal(TreeNode* root) {
         vector<int>inorder ;
-        display(root , inorder);
+        // display(root , inorder);
+        TreeNode* curr = root ;
+        while(curr != NULL){
+            if(curr->left==NULL){
+                inorder.push_back(curr->val);
+                curr = curr->right ;
+            }
+            else{
+                TreeNode* prev = curr->left ;
+                while(prev->right && prev->right != curr){
+                    prev = prev->right ;
+                }
+                if(prev->right == NULL){
+                    prev->right = curr ;
+                    curr = curr->left ;
+                }else{
+                    prev->right = NULL;
+                    inorder.push_back(curr->val);
+                    curr = curr->right ;
+                }
+            }
+        }
         return inorder ;
     }
 };
