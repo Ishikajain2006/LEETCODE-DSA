3e244b61cce36be48aabc1b1518f8ed527950c92
ISHIKA JAIN <pawanishika10@gmail.com>
2026-09-06 20:29:46 +0530

    Time: 147 ms (19.13%) | Memory: 91 MB (38.15%) - LeetSync

---
+++ 4401-sum-of-decoded-numbers/sum-of-decoded-numbers.cpp
@@ -0,0 +1,37 @@
+#include<cmath>
+class Solution {
+public:
+    long long power(long long x, long long y) { 
+        long long ans = 1; 
+        long long MOD = 1e9 + 7; 
+        while (y > 0) {
+            if (y % 2 == 1)  ans = (ans * x) % MOD;
+            x = (x * x) % MOD; 
+            y /= 2; 
+        } 
+        return ans; 
+    }
+    int sumDecoded(vector<long long>& nums) {
+        long long sum = 0; 
+        long long MOD = 1e9 + 7; 
+        for (int i = 0; i < nums.size(); i++) { 
+            long long num = nums[i]; 
+            int width = num % 10; 
+            long long d = num / 10; 
+            string s = to_string(d); 
+            string x = "", y = ""; 
+            int j = 0; 
+            while (j < s.length()) { 
+                if (width > 0) { 
+                    x += s[j]; 
+                    width--; 
+                } else y += s[j]; 
+                    j++; 
+            } 
+            long long base = stoll(x); 
+            long long exponent = stoll(y); 
+            sum = (sum + power(base, exponent)) % MOD; 
+        } 
+        return sum;
+    }
+};
