a96be342865d5ad4f880a486b772cbfcf77ca509
ISHIKA JAIN <pawanishika10@gmail.com>
2026-09-07 09:47:12 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (11.33%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -3,16 +3,23 @@ public:
     bool isAnagram(string s, string t) {
         if(s.length()!=t.length()) return false ;
         unordered_map<char , int>mps(0) ;
-        unordered_map<char , int>mpt(0);
         for(char ch : s) mps[ch]++ ;
-        for(char ch : t) mpt[ch]++ ;
-        for(auto m : mps){
-            char ch = m.first ;
-            int freq1 = m.second;
-            if(mpt.find(ch)!=mpt.end()){
-                if(freq1 != mpt[ch]) return false ;
+        // unordered_map<char , int>mpt(0);
+        // for(char ch : t) mpt[ch]++ ;
+        // for(auto m : mps){
+        //     char ch = m.first ;
+        //     int freq1 = m.second;
+        //     if(mpt.find(ch)!=mpt.end()){
+        //         if(freq1 != mpt[ch]) return false ;
+        //     }else return false ;
+        // }
+         for(char ch : t){
+            if(mps.find(ch)!=mps.end()){
+                mps[ch]-- ;
+                if(mps[ch]==0) mps.erase(ch);
             }else return false ;
-        }
-        return true ;
+         }
+
+        return mps.empty() ;
     }
 };
