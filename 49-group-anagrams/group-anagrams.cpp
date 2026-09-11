423e0db7ca9e64337a25e023e775bed230357422
ISHIKA JAIN <pawanishika10@gmail.com>
2026-09-11 20:51:13 +0530

    Time: 19 ms (44.82%) | Memory: 26.2 MB (43.52%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -6,13 +6,12 @@ public:
         for(int i=0 ; i<n ; i++){
             string lexo = strs[i];
             sort(lexo.begin() , lexo.end());
-            if(mp.find(lexo)!=mp.end()) mp[lexo].push_back(strs[i]);
-            else mp[lexo].push_back(strs[i]);
+            mp[lexo].push_back(strs[i]);
+            // if(mp.find(lexo)!=mp.end()) mp[lexo].push_back(strs[i]);
+            // else mp[lexo].push_back(strs[i]);
         }
         vector<vector<string>> ans ;
-        for(auto ele : mp){
-            ans.push_back(ele.second);
-        }
+        for(auto ele : mp)  ans.push_back(ele.second);
         return ans ;
     }
 };
