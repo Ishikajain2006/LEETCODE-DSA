cbd5bc668900de2a52b07d434578ed1b96dfe371
ISHIKA JAIN <pawanishika10@gmail.com>
2026-09-11 20:44:45 +0530

    Time: 16 ms (59.21%) | Memory: 26.1 MB (43.52%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string , vector<string>>mp ;
+        int n = strs.size();
+        for(int i=0 ; i<n ; i++){
+            string lexo = strs[i];
+            sort(lexo.begin() , lexo.end());
+            if(mp.find(lexo)!=mp.end()) mp[lexo].push_back(strs[i]);
+            else mp[lexo].push_back(strs[i]);
+        }
+        vector<vector<string>> ans ;
+        for(auto ele : mp){
+            ans.push_back(ele.second);
+        }
+        return ans ;
+    }
+};
